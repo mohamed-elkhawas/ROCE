d6daa1625afab89fde70b0bc04371fee47f24ec2
mohamed-elkhawas <65501370+mohamed-elkhawas@users.noreply.github.com>
2021-04-04 03:52:06 +0200

    v1.0001

--- TX_controller/verilog/returner_tb.v
+++ TX_controller/verilog/returner_tb.v
@@ -1,40 +1,34 @@
 module returner_tb ();
  
 
-//		valid + address
-//reg [0:63][ 1 + 31 : 0]	read_return_array;
-
-//		valid + address + data
-//reg [0:63]						write_return_array;
-
-
 reg clk,rst;
 wire  [ 31 : 0 ] data;
-
+wire wd,rd;
 returner r (clk,rst,wd,rd,data);
-
-
-
-
+  
+  
 // Clock generator
   always
   begin
     #1 clk = 1;
     #1 clk = 0;
   end
+  
 
 
 initial begin 
+  $dumpfile("dump.vcd"); $dumpvars;
 
 	# 10 rst <= 0;
 	# 10 rst <= 1;
 
-	#10 r.write_return_array[0] =1;
+	#11 r.write_return_array[0] =1;
 	#10 r.write_return_array[1] =1;
 
 	#10 r.write_return_array[4] =1;
 	#10 r.write_return_array[3] =1;
 
+  #100 $finish;
 end
 
 endmodule
