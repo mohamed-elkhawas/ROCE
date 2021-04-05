efe7c5dd252db3a2b89814d00f6217a7c6b24080
mohamed-elkhawas <65501370+mohamed-elkhawas@users.noreply.github.com>
2021-04-05 22:29:58 +0200

    v1.0004

--- TX_controller/system verilog/returner.sv
+++ TX_controller/system verilog/returner.sv
@@ -62,7 +62,7 @@ always_comb begin
 end
 
 
-always_comb begin //////////////////////////  don't know if it will work ///////////////////////////////
+always_comb begin 
 	case (curr_state)
 		
 		working : begin
