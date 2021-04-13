ddc02a7e0470feeb81c608fcd36f97e6e4a7ff76
mohamed-elkhawas <65501370+mohamed-elkhawas@users.noreply.github.com>
2021-04-13 15:18:31 +0200

    Update global_array.sv

--- design/front_end/global_array.sv
+++ design/front_end/global_array.sv
@@ -5,12 +5,16 @@ module global_array import types_def::*;
 (
 	input clk,    
 	input rst,
-	input request in_request,
-	input [1:0] in_enable,  // en [0] mapper  en[1] scheduler
-	input [1:0] done,  		// done [0] read  done[1] write
-	output request out_request,
-	output logic sending,
-	output logic [1:0] busy_out
+	input request in_request,	// from mapper
+	input logic [read_entries_log:0] in_request_index, // from mapper
+	input logic [1:0] in_enable,	  	// en [0] mapper , en[1] scheduler
+	input r_type the_mapper_type,  	// the mapper request type
+	input [1:0] done,  			// done [0] read_done  done[1] write_done from returner
+	output request out_request, // to scheduler
+	output logic [read_entries_log:0] out_request_index, // to scheduler
+	output r_type the_scheduler_type,  	// the mapper request type
+	output logic sending, 		// to scheduler
+	output logic [1:0] busy_out // busy read and write
 	
 );
 
@@ -24,8 +28,52 @@ logic [0:read_entries][ 1 + address_width : 0]					read_global_array;
 //		valid + address + data
 logic [0:write_entries][ 1 + address_width + data_width + 1 : 0]	write_global_array;
 
-logic [read_entries_log:0] the_read_counter ;
-logic [write_entries_log:0] the_write_counter ;
+logic [read_entries_log:0] the_diffrence_in_read_counters ;
+logic [write_entries_log:0] the_diffrence_in_write_counters ;
+
+always_ff @(posedge clk  ) begin
+	if (rst) begin
+		if (done [0]) begin
+			the_diffrence_in_read_counters --;
+		end	
+		if (done [1]) begin
+			the_diffrence_in_write_counters --;
+		end
+		if (in_enable[0]) begin // mapper is sending req
+			if (the_mapper_type == read) begin
+				the_diffrence_in_read_counters ++;
+			end
+			else begin
+				the_diffrence_in_write_counters ++;
+			end
+			
+		end
+		
+	end
+	else begin
+		the_diffrence_in_read_counters = 0;
+		the_diffrence_in_write_counters = 0;
+	end
+	
+end
+
+always_comb begin 
+	if (the_diffrence_in_read_counters == read_entries) begin
+				busy_out[0] = 1;
+	end	
+	else begin
+		busy_out[0] = 0;
+	end
+	if (the_diffrence_in_write_counters == write_entries) begin
+				busy_out[1] = 1;
+	end	
+	else begin
+		busy_out[1] = 0;
+	end
+
+end
+
+//////////////////////////// to be continued
 
 always_ff @(posedge clk ) begin
 	if(rst) begin
@@ -36,43 +84,10 @@ always_ff @(posedge clk ) begin
 	end
 end
 
-always_ff @(posedge clk  ) begin
-	if (rst) begin
-		if (done [0]) begin
-			the_read_counter --;
-		end	
-		if (done [1]) begin
-			the_write_counter --;
-		end
-		
-	end
-	else begin
-		the_read_counter = 0;
-		the_write_counter = 0;
-	end
-	
-end
-
-always_comb begin 
-	if (the_read_counter == read_entries) begin
-				busy_out[0] = 1;
-	end	
-	else begin
-		busy_out[0] = 0;
-	end
-	if (the_write_counter == write_entries) begin
-				busy_out[1] = 1;
-	end	
-	else begin
-		busy_out[1] = 0;
-	end
-
-end
-//////////////////////////// to be continued
 always_comb begin
 
 	if (in_enable != 0) begin
-
+		next_state = working ;
 	end	
 	
 	else begin
@@ -87,11 +102,13 @@ always_comb begin
 			
 		end
 		idle : begin
-			
+			sending = 0;
+			out_request = 0;
 		end
 
 		reset_state : begin
 			sending = 0;
+			out_request = 0;
 		end
 
 		default : begin
