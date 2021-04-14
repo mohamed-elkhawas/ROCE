961e6692b6df773fdde2b383645e7a66ae7fce94
mohamed-elkhawas <65501370+mohamed-elkhawas@users.noreply.github.com>
2021-04-14 15:46:39 +0200

    Update global_array.sv

--- design/front_end/global_array.sv
+++ design/front_end/global_array.sv
@@ -1,24 +1,27 @@
 
-
 module global_array import types_def::*;
 
 (
 	input clk,    
 	input rst,
-	input request in_request,	// from mapper
-	input logic [read_entries_log:0] in_request_index, // from mapper
-	input r_type the_mapper_type,  	// the mapper request type
+	input request in_request,							// from mapper
+	input logic [read_entries_log:0] in_request_index,  // from mapper
+	input r_type the_mapper_req_type,  					// the mapper request type
+	input mapper_enable,								// from mapper
+	output logic stop_reading, 							// to mapper busy read 
+	output logic stop_writing,							// to mapper busy write
 
-	input logic [1:0] in_enable,	  	// en [0] mapper , en[1] scheduler
+	input logic [1:0] in_enable,	  					// en [0] mapper , en[1] scheduler
 
+	input scheduler_enable,								// from scheduler
 	input logic [read_entries_log:0] out_request_index, // from scheduler
-	input r_type the_scheduler_type,  	// the scheduler request type
+	input r_type the_scheduler_req_type,  				// the scheduler request type
 
-	input [1:0] done,  			// done [0] read_done  done[1] write_done from returner
+	input  read_done,  									// read_done read_done  write_done write_done from returner
+	input  write_done,
 	
-	output request out_request, // to scheduler
-	output logic sending, 		// to scheduler
-	output logic [1:0] busy_out // busy read and write
+	output request out_request, 						// to scheduler
+	output logic sending 								// to scheduler
 	
 );
 
@@ -27,57 +30,11 @@ typedef enum logic [1:0] {idle,working,reset_state} my_states ;
 my_states curr_state , next_state ; 
 
 //		valid + address
-logic [0:read_entries][ 1 + address_width : 0]				read_global_array;
+logic [0:read_entries][ 1 + address_width : 0]					read_global_array;
 
 //		valid + address + data
 logic [0:write_entries][ 1 + address_width + data_width + 1 : 0]	write_global_array;
 
-logic [read_entries_log:0] the_diffrence_in_read_counters ;
-logic [write_entries_log:0] the_diffrence_in_write_counters ;
-
-always_ff @(posedge clk  ) begin
-	if (rst) begin
-		if (done [0]) begin
-			the_diffrence_in_read_counters --;
-		end	
-		if (done [1]) begin
-			the_diffrence_in_write_counters --;
-		end
-		if (in_enable[0]) begin // mapper is sending req
-			if (the_mapper_type == read) begin
-				the_diffrence_in_read_counters ++;
-			end
-			else begin
-				the_diffrence_in_write_counters ++;
-			end
-			
-		end
-		
-	end
-	else begin
-		the_diffrence_in_read_counters = 0;
-		the_diffrence_in_write_counters = 0;
-	end
-	
-end
-
-always_comb begin 
-	if (the_diffrence_in_read_counters == read_entries) begin
-				busy_out[0] = 1;
-	end	
-	else begin
-		busy_out[0] = 0;
-	end
-	if (the_diffrence_in_write_counters == write_entries) begin
-				busy_out[1] = 1;
-	end	
-	else begin
-		busy_out[1] = 0;
-	end
-
-end
-
-//////////////////////////// to be continued
 
 always_ff @(posedge clk ) begin
 	if(rst) begin
@@ -90,7 +47,7 @@ end
 
 always_comb begin
 
-	if (in_enable != 0) begin
+	if (mapper_enable || scheduler_enable) begin
 		next_state = working ;
 	end	
 	
@@ -103,7 +60,34 @@ always_comb begin
 	case (curr_state)
 		
 		working : begin
-			
+			if (mapper_enable) begin
+
+				if (the_mapper_req_type == read) begin
+					read_global_array [in_request_index] = in_request ;
+				end
+				else begin
+					write_global_array [in_request_index] = in_request ;
+				end
+
+			end
+
+			if (scheduler_enable) begin
+
+				if (the_scheduler_req_type == read) begin
+					out_request = read_global_array [out_request_index];
+					sending = 1;
+				end
+				else begin
+					out_request = write_global_array [out_request_index];
+					sending = 1;
+				end
+
+			end
+
+			else begin
+				out_request = 0;
+				sending = 0;
+			end
 		end
 		idle : begin
 			sending = 0;
