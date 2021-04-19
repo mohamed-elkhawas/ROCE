8ab414eb8d6f3bac6e53e037d97de2b3b9a38d75
mohamed-elkhawas <65501370+mohamed-elkhawas@users.noreply.github.com>
2021-04-19 22:15:34 +0200

    Update returner_tb.sv

--- test_bench/front_end/TXN_controller/returner_tb.sv
+++ test_bench/front_end/TXN_controller/returner_tb.sv
@@ -1,24 +1,21 @@
 module returner_tb ();
  
-/*
-//		valid + address
-logic [0:63][ 1 + 31 : 0]	read_return_array;
 
-//		valid + address + data
-logic [0:63]						write_return_array;
-
-*/
 logic clk,rst,wd,rd;
 
-logic  [ 31 : 0 ] data;
+logic the_type;
+logic valid;
+logic  [ 31 : 0 ] in_data;
+logic  [ 5 : 0 ] index;
 
-returner r (clk,rst,wd,rd,data);
+
+logic  [ 31 : 0 ] out_data;
 
 
-task reset();
-	# 10 rst <= 0;
-	# 10 rst <= 1;
-endtask 
+returner r (clk,rst,valid,the_type,in_data,index,wd,rd,data);
+
+
+
 
 // Clock generator
   always
@@ -30,13 +27,87 @@ endtask
 
 initial begin 
 
-	reset();
+	rst = 0;
+	# 10 rst = 1;
+	#10
+	
+	valid =1;
+	the_type =0;
+	in_data =1;
+	index =0; /////////0
+	#2
+	valid =0;
 
-	#100 r.write_return_array[0] =1;
-	#100 r.write_return_array[1] =1;
 
-	#100 r.write_return_array[4] =1;
-	#100 r.write_return_array[3] =1;
+	#100 
+	valid =1;
+	the_type =0;
+	in_data =2;
+	index =1; /////////1
+	#2
+	valid =0;
+
+	#100 
+	valid =1;
+	the_type =0;
+	in_data =3;
+	index =3; /////////3
+	#2
+	valid =0;
+
+	#100 
+	valid =1;
+	the_type =0;
+	in_data =4;
+	index =2; /////////2
+	#2
+	valid =0;
+	
+	#100 
+
+	valid =1;
+	the_type =1;
+	in_data =1;
+	index =0; /////////0
+	#2
+	valid =0;
+
+
+	#100 
+	valid =1;
+	the_type =1;
+	in_data =1;
+	index =1; /////////1
+	#2
+	valid =0;
+
+	#100 
+	valid =1;
+	the_type =1;
+	in_data =1;
+	index =0; /////////4
+	#2
+	valid =0;
+
+	#100 
+	valid =1;
+	the_type =1;
+	in_data =1;
+	index =3; /////////3
+	#2
+	valid =0;
+
+
+	#100 
+	valid =1;
+	the_type =1;
+	in_data =1;
+	index =2; /////////2
+	#2
+	valid =0;
+
+	#100
+	$stop;
 
 end
 
