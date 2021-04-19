5816028ea1cf29c20fc40c1fd687f174eb4305ab
mohamed-elkhawas <65501370+mohamed-elkhawas@users.noreply.github.com>
2021-04-19 22:10:19 +0200

    Update global_array.sv

--- design/front_end/global_array.sv
+++ design/front_end/global_array.sv
@@ -4,24 +4,19 @@ module global_array import types_def::*;
 (
 	input clk,    
 	input rst,
+	
 	input request in_request,							// from mapper
 	input logic [read_entries_log:0] in_request_index,  // from mapper
-	input r_type the_mapper_req_type,  					// the mapper request type
-	input mapper_enable,								// from mapper
+	input mapper_valid,									// from mapper
 	output logic stop_reading, 							// to mapper busy read 
 	output logic stop_writing,							// to mapper busy write
 
-	input logic [1:0] in_enable,	  					// en [0] mapper , en[1] scheduler
-
-	input scheduler_enable,								// from scheduler
+	input scheduler_valid,								// from scheduler
 	input logic [read_entries_log:0] out_request_index, // from scheduler
 	input r_type the_scheduler_req_type,  				// the scheduler request type
 
-	input  read_done,  									// read_done read_done  write_done write_done from returner
-	input  write_done,
-	
 	output request out_request, 						// to scheduler
-	output logic sending 								// to scheduler
+	output logic out_request_valid 								// to scheduler
 	
 );
 
@@ -29,11 +24,19 @@ typedef enum logic [1:0] {idle,working,reset_state} my_states ;
 
 my_states curr_state , next_state ; 
 
-//		valid + address
-logic [0:read_entries][ 1 + address_width : 0]					read_global_array;
 
-//		valid + address + data
-logic [0:write_entries][ 1 + address_width + data_width + 1 : 0]	write_global_array;
+typedef struct packed {
+	logic [address_width:0] address ;
+	logic [data_width:0] data ;
+
+} request_without_type;
+
+
+//		address
+logic [0:read_entries][ address_width : 0]					read_global_array;
+
+//		 address + data
+request_without_type [0:write_entries]					write_global_array;
 
 
 always_ff @(posedge clk ) begin
@@ -47,7 +50,7 @@ end
 
 always_comb begin
 
-	if (mapper_enable || scheduler_enable) begin
+	if (mapper_valid || scheduler_valid) begin
 		next_state = working ;
 	end	
 	
@@ -60,47 +63,50 @@ always_comb begin
 	case (curr_state)
 		
 		working : begin
-			if (mapper_enable) begin
+			if (mapper_valid) begin
 
-				if (the_mapper_req_type == read) begin
-					read_global_array [in_request_index] = in_request ;
+				if (in_request.req_type == read) begin
+					read_global_array [in_request_index] = in_request.address ;
 				end
 				else begin
-					write_global_array [in_request_index] = in_request ;
+					write_global_array [in_request_index].address = in_request.address ;
+					write_global_array [in_request_index].data = in_request.data ;
 				end
 
 			end
 
-			if (scheduler_enable) begin
+			if (scheduler_valid) begin
 
 				if (the_scheduler_req_type == read) begin
-					out_request = read_global_array [out_request_index];
-					sending = 1;
+					out_request.address = read_global_array [out_request_index];
+					out_request_valid = 1;
 				end
 				else begin
-					out_request = write_global_array [out_request_index];
-					sending = 1;
+					out_request.address = write_global_array [out_request_index].address;
+					out_request.data = write_global_array [out_request_index].data;
+					out_request_valid = 1;
 				end
 
 			end
 
 			else begin
 				out_request = 0;
-				sending = 0;
+				out_request_valid = 0;
 			end
 		end
 		idle : begin
-			sending = 0;
+			out_request_valid = 0;
 			out_request = 0;
 		end
 
 		reset_state : begin
-			sending = 0;
+			out_request_valid = 0;
 			out_request = 0;
 		end
 
 		default : begin
-			
+			out_request_valid = 0;
+			out_request = 0;
 		end
 	
 	endcase
