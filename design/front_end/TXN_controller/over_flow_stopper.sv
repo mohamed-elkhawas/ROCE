4cf0d75979a59ab32ded463836483dbd7c847d3d
mohamed-elkhawas <65501370+mohamed-elkhawas@users.noreply.github.com>
2021-05-02 05:33:21 +0200

    Update over_flow_stopper.sv

--- design/front_end/TXN_controller/over_flow_stopper.sv
+++ design/front_end/TXN_controller/over_flow_stopper.sv
@@ -15,8 +15,8 @@ module over_flow_stopper import types_def::*;
 
 );
 
-logic [read_entries_log+1:0] the_diffrence_in_read_counters ;
-logic [write_entries_log+1:0] the_diffrence_in_write_counters ;
+logic [read_entries_log -1 +1:0] diff_read_counter ;
+logic [write_entries_log -1 +1:0] diff_write_counter ;
 
 
 always_ff @(posedge clk  ) begin
@@ -26,13 +26,13 @@ always_ff @(posedge clk  ) begin
 
 			if (the_req_type == read ) begin
 				if (read_done == 0) begin
-					the_diffrence_in_read_counters ++;
+					diff_read_counter ++;
 				end	
 			end
 
 			else begin
 				if (write_done == 0) begin
-					the_diffrence_in_write_counters ++;
+					diff_write_counter ++;
 				end
 			end
 			
@@ -40,36 +40,39 @@ always_ff @(posedge clk  ) begin
 
 		else begin
 			if (read_done) begin
-				the_diffrence_in_read_counters --;
+				diff_read_counter --;
 			end	
 			if (write_done) begin
-				the_diffrence_in_write_counters --;
+				diff_write_counter --;
 			end
 		end
 	end
 
 	else begin
-		the_diffrence_in_read_counters = 0;
-		the_diffrence_in_write_counters = 0;
+		diff_read_counter = 0;
+		diff_write_counter = 0;
 	end
 
 end
 
 always_comb begin 
 
-	if (the_diffrence_in_read_counters == read_entries +1 ) begin
+	if (diff_read_counter == read_entries -1 +1 ) begin
 		stop_reading = 1;
 	end	
 	else begin
 		stop_reading = 0;
 	end
 
-	if (the_diffrence_in_write_counters == write_entries +1 ) begin
+	if (diff_write_counter == write_entries +1 ) begin
 		stop_writing = 1;
 	end	
 	else begin
 		stop_writing = 0;
 	end
+
 end
 
+
 endmodule
+
