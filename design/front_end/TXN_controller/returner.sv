8b782f9ecd86d3bde9c539af9034a17314edb25d
mohamed-elkhawas <65501370+mohamed-elkhawas@users.noreply.github.com>
2021-05-11 16:54:55 +0200

    meeting modifications

--- design/front_end/TXN_controller/returner.sv
+++ design/front_end/TXN_controller/returner.sv
@@ -6,7 +6,7 @@ module returner  import types_def::*;
 
 	(
 	input clk,    	// Clock
-	input rst,  	// synchronous reset active low
+	input rst_n,  	// synchronous reset active low
 	input valid,
 	input the_type,
 	input [ data_width -1  : 0 ] data_in,
@@ -35,41 +35,29 @@ logic [0:63][ 1 + data_width -1 : 0]	read_return_array;
 logic [0:63]						write_return_array;
 
 
-typedef struct packed {
-	logic the_type ;
-	logic [ data_width -1  : 0 ] data_in;
-	logic [ read_entries_log -1 : 0 ] index;
-	logic valid;
-
-} previous_input_type;
-
-previous_input_type previous_input;
-
-
-task save_the_input ();
-	previous_input.the_type <= the_type;
-	previous_input.data_in <= data_in;
-	previous_input.index <= index;
-	previous_input.valid <= valid;
-endtask
-
-
 logic read_counter_up , write_counter_up;
 
 
 always_ff @(posedge clk ) begin
-	if(rst) begin
+	if(rst_n) begin
 		curr_state <= next_state ;
-		save_the_input ();
-		if(read_counter_up) begin
-			read_counter ++;
-		end
-		if(write_counter_up) begin
-			write_counter ++;
-		end
 	end 
 	else begin
 		curr_state <= reset_s;
+	end
+end
+
+
+always_ff @(posedge clk ) begin
+	if(rst_n) begin
+		if(read_counter_up) begin
+			read_counter <= read_counter +1;
+		end
+		if(write_counter_up) begin
+			write_counter <= write_counter +1;
+		end
+	end 
+	else begin
 		read_counter <= 0;
 		write_counter <= 0;
 	end
@@ -84,7 +72,7 @@ always_comb begin
 				next_state = working_with_new_req ;
 			end
 			else begin
-					next_state = idle ;
+				next_state = idle ;
 			end
 		end
 		reset_s: begin
@@ -92,11 +80,11 @@ always_comb begin
 				next_state = working_with_new_req ;
 			end
 			else begin
-					next_state = idle ;
+				next_state = idle ;
 			end
 		end
 		working_with_new_req: begin
-			if (read_return_array[read_counter][0] == 1 || write_return_array[write_counter] == 1 ) begin
+			if ((read_return_array[read_counter +1][0] == 1 && read_counter_up) || (write_return_array[write_counter +1] == 1 && write_counter_up )) begin
 				next_state = working_with_old_req ;
 			end
 			else begin
@@ -109,7 +97,7 @@ always_comb begin
 			end
 		end
 		working_with_old_req: begin
-			if (read_return_array[read_counter][0] == 1 || write_return_array[write_counter] == 1 ) begin
+			if ((read_return_array[read_counter +1][0] == 1 && read_counter_up) || (write_return_array[write_counter +1] == 1 && write_counter_up )) begin
 				next_state = working_with_old_req ;
 			end
 			else begin
@@ -128,68 +116,69 @@ end
 
 
 
-
-always_comb begin 
+always_ff @(posedge clk ) begin 
 
 	read_counter_up = 0;
 	write_counter_up = 0;
 
-	case (curr_state)
+	case (next_state)
 		
 		working_with_new_req : begin
-			if (previous_input.the_type == read) begin
-				if ( read_counter == previous_input.index ) begin
-					data_out = previous_input.data_in ;
+			if (the_type == read) begin
+				if ( read_counter == index ) begin
+					data_out = data_in ;
 					read_done = 1 ;
 					read_counter_up = 1;
 				end
-
 				else begin
 					read_done = 0;
 					data_out = 0;
-					read_return_array [previous_input.index] = { 1 , previous_input.data_in };
+					read_return_array [index] = { 1 , data_in };
 				end					
 			end
 				
 			else begin
-				if ( write_counter == previous_input.index ) begin
+				if ( write_counter == index ) begin
 					write_done = 1 ;
 					write_counter_up = 1;
 				end
-
 				else begin
 					write_done = 0;
-					write_return_array [previous_input.index] = 1;
+					write_return_array [index] = 1;
 				end	
 			end
 		end
 
 		working_with_old_req : begin
-			if (read_return_array [read_counter][0] == 1) begin
-				data_out = read_return_array [read_counter][data_width -1:0];
+			
+			if (read_return_array [read_counter +1][0] == 1) begin
+				data_out = read_return_array [read_counter +1][data_width -1:0];
 				read_done =1;
 				read_counter_up = 1;
+				read_return_array [read_counter +1] = 0;
 			end
 			else begin
 				data_out = 0;
 				read_done = 0;
 			end
-			if (write_return_array [write_counter] == 1) begin
+
+			if (write_return_array [write_counter +1] == 1) begin
 				write_done = 1 ;
-				write_return_array [write_counter] = 0 ;
+				write_return_array [write_counter +1] = 0 ;
 				write_counter_up = 1;					
 			end
 			else begin
 				write_done = 0;
 			end
-
-			if (previous_input.the_type == read) begin
-				read_return_array [previous_input.index] = { 1 , previous_input.data_in };
+			if (valid) begin
+				if (the_type == read) begin
+					read_return_array [index] = { 1 , data_in };
+				end	
+				else begin
+					write_return_array [index] = 1;
+				end
 			end
-				
-			else begin
-				write_return_array [previous_input.index] = 1;
-			end
+			
 		end
 		
 		idle : begin
@@ -212,6 +201,5 @@ always_comb begin
 	
 	endcase
 end
-
 
 endmodule
