2f41711589836fe75ed801a391988df23a02f989
mohamed-elkhawas <65501370+mohamed-elkhawas@users.noreply.github.com>
2021-05-11 16:56:57 +0200

    Update txn_controller.sv

--- design/front_end/TXN_controller/txn_controller.sv
+++ design/front_end/TXN_controller/txn_controller.sv
@@ -4,7 +4,7 @@ module txn_controller import types_def::*;
  
  (
 	input clk,    // Clock
-	input rst,  // synchronous reset active low
+	input rst_n,  // synchronous reset active low
 	
 //////////////////// the mapper \\\\\\\\\\\\\\\\\\\\\\\\\
 
@@ -34,13 +34,13 @@ module txn_controller import types_def::*;
 );
 
 
-mapper input_controller (.clk(clk),.rst(rst),.in_valid(in_valid),.in_request(in_request),.out_busy(out_busy),.stop_reading(stop_reading) ,.stop_writing(stop_writing),.valid_out(valid_out),.out_req(out_req),.out_index(out_index),.in_busy(in_busy),.bank_out_valid(bank_out_valid));
+mapper input_controller (.clk(clk),.rst_n(rst_n),.in_valid(in_valid),.in_request(in_request),.out_busy(out_busy),.stop_reading(stop_reading) ,.stop_writing(stop_writing),.valid_out(valid_out),.out_req(out_req),.out_index(out_index),.in_busy(in_busy),.bank_out_valid(bank_out_valid));
 
 
-returner #(.data_width(data_width)) output_controller (.clk(clk),.rst(rst),.valid(request_done_valid),.the_type(the_type),.data_in(data_in),.index(index),.write_done(write_done),.read_done(read_done),.data_out(data_out));
+returner #(.data_width(data_width)) output_controller (.clk(clk),.rst_n(rst_n),.valid(request_done_valid),.the_type(the_type),.data_in(data_in),.index(index),.write_done(write_done),.read_done(read_done),.data_out(data_out));
 
 
-over_flow_stopper the_over_flow_stopper (.clk(clk),.rst(rst),.mapper_valid(valid_out),.the_req_type(out_req.req_type),.read_done(read_done),.write_done(write_done),.stop_reading(stop_reading),.stop_writing(stop_writing));
+over_flow_stopper the_over_flow_stopper (.clk(clk),.rst_n(rst_n),.mapper_valid(valid_out),.the_req_type(out_req.req_type),.read_done(read_done),.write_done(write_done),.stop_reading(stop_reading),.stop_writing(stop_writing));
 
 
 endmodule
