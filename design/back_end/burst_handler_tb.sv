d0b5bd320288851a7d5a18e5a8b7cd90604b99b1
mohamed-elkhawas <65501370+mohamed-elkhawas@users.noreply.github.com>
2021-06-09 04:06:16 +0200

    Update and rename memory_s_best_friend_tb.sv to burst_handler_tb.sv

--- design/back_end/burst_handler_tb.sv
+++ design/back_end/burst_handler_tb.sv
@@ -1,11 +1,11 @@
-module memory_s_best_friend_tb  import types_def::*; ();
+module burst_handler_tb  import types_def::*; ();
 
 
 parameter no_of_bursts  = 4;
 
 logic clk,rst_n;
-command [no_of_bursts-1:0] in_burst_cmd;
-
+command in_burst_cmd;
+logic [1:0] in_cmd_index;
 
 
 logic arbiter_valid;
@@ -17,7 +17,7 @@ r_type arbiter_type;
 logic test;
 
 memory_s_best_friend m (clk,rst_n,out_burst_state,out_burst_type,
-	out_burst_address,in_burst_cmd,start_new_burst,arbiter_valid,in_req_address,
+	out_burst_address,in_burst_cmd,in_cmd_index,start_new_burst,arbiter_valid,in_req_address,
 	arbiter_data,arbiter_index,arbiter_type,returner_valid,returner_type,returner_data,returner_index);//,test);
 
 
@@ -43,9 +43,8 @@ initial begin
 	arbiter_index =0;
 
 	#11
-	$monitor($time(),"	returner_valid = %b , index = %b",returner_valid,returner_index);
 	
-	@(posedge clk)///////////////////r
+	@(posedge clk)///////////////////read one burst two reqs
 	arbiter_valid=1;
 	in_req_address.column[3:0] =0;
 	arbiter_index =0;
@@ -55,14 +54,14 @@ initial begin
 	@(posedge clk)
 	arbiter_valid=0;
 
-	@(posedge clk)///////////////////r
+	@(posedge clk)///////////////////read
 	arbiter_valid=1;
 	@(posedge clk)
 	arbiter_valid=0;
 
-	@(posedge clk)///////////////////r
+	@(posedge clk)///////////////////read
 	arbiter_valid=1;
-	@(posedge clk)///////////////////w
+	@(posedge clk)///////////////////write
 	arbiter_type = write;
 	@(posedge clk)
 	arbiter_valid=0;
