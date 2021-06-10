fa67ae2b793fbfbbae43effd03900fa5e9ac4138
mohamed-elkhawas <65501370+mohamed-elkhawas@users.noreply.github.com>
2021-06-10 15:55:27 +0200

    Update txn_controller.sv

--- design/front_end/TXN_controller/txn_controller.sv
+++ design/front_end/TXN_controller/txn_controller.sv
@@ -10,13 +10,13 @@ module txn_controller import types_def::*;
 
 	input in_valid, 	// from rnic
 	input request in_request, // from rnic
-	output logic out_busy, // to rnic
+	output logic out_busy2, // to rnic
 
-	output opt_request out_req,// to bank
-	output logic [read_entries_log -1:0] out_index,// to the bank
+	output opt_request out_req2,// to bank fifo
+	output logic [read_entries_log -1:0] out_index2,// to the bank fifo
 
-	input [15:0] in_busy, // from bank
-	output logic  [15:0] bank_out_valid, // to bank 
+	input [15:0] fifo_grant_o, // from bank fifo
+	output logic  [15:0] bank_out_valid2, // to bank fifo
 
 ///////////////////// the returner \\\\\\\\\\\\\\\\\\\\\\\\\
 								  
@@ -31,8 +31,11 @@ module txn_controller import types_def::*;
 	
 );
 
+request out_req;
+logic [ read_entries_log -1 : 0 ] out_index;
+logic  [15:0] bank_out_valid;
 
-mapper input_controller (.clk(clk),.rst_n(rst_n),.in_valid(in_valid),.in_request(in_request),.out_busy_o(out_busy),.stop_reading(stop_reading) ,.stop_writing(stop_writing),.valid_out_o(valid_out),.out_req_o(out_req),.out_index_o(out_index),.in_busy(in_busy),.bank_out_valid_o(bank_out_valid));
+mapper input_controller (.clk(clk),.rst_n(rst_n),.in_valid(in_valid),.in_request(in_request),.out_busy_o(out_busy),.stop_reading((~grant_o2|stop_reading)) ,.stop_writing((~grant_o2|stop_writing)),.valid_out_o(valid_out),.out_req_o(out_req),.out_index_o(out_index),.in_busy(~fifo_grant_o),.bank_out_valid_o(bank_out_valid));
 
 
 returner #(.data_width(data_width)) output_controller (.clk(clk),.rst_n(rst_n),.valid(request_done_valid),.the_type(the_type),.data_in(data_in),.index(index),.write_done(write_done),.read_done(read_done),.data_out(data_out));
@@ -40,5 +43,10 @@ returner #(.data_width(data_width)) output_controller (.clk(clk),.rst_n(rst_n),.
 
 over_flow_stopper the_over_flow_stopper (.clk(clk),.rst_n(rst_n),.mapper_valid(valid_out),.the_req_type(out_req.req_type),.read_done(read_done),.write_done(write_done),.stop_reading(stop_reading),.stop_writing(stop_writing));
 
+
+request_saver the_request_saver (.clk(clk),.rst_n(rst_n),.request_i(out_req),.valid_i(bank_out_valid),.index_i(out_index),.grant_o2(grant_o2),.grant_o(fifo_grant_o),.request_i2(out_req2),.valid_i2(bank_out_valid2),.index_i2(out_index2));
+
+
+assign out_busy2 = ( (~grant_o2) || (!fifo_grant_o) || out_busy || stop_reading || stop_writing ) ;
 
 endmodule
