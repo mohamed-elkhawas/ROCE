de12cbde59936838b890c227a17f663b8b61b9c2
mohamed-elkhawas <65501370+mohamed-elkhawas@users.noreply.github.com>
2021-07-18 16:10:43 +0200

    Update burst_handler_tb.sv

--- Mem_Controller/design/back_end/burst_handler_tb.sv
+++ Mem_Controller/design/back_end/burst_handler_tb.sv
@@ -3,23 +3,66 @@ module burst_handler_tb  import types_def::*; ();
 
 parameter no_of_bursts  = 4;
 
-logic clk,rst_n;
-command in_burst_cmd;
-logic [1:0] in_cmd_index;
-
-
-logic arbiter_valid;
-address_type in_req_address;
-logic  [data_width -1:0] arbiter_data;
-logic [read_entries_log -1:0] arbiter_index;
-r_type arbiter_type;
 
 logic test;
 
-burst_handler m (clk,rst_n,out_burst_state,out_burst_type,
-	out_burst_address,in_burst_cmd,in_cmd_index,start_new_burst,arbiter_valid,in_req_address,
-	arbiter_data,arbiter_index,arbiter_type,returner_valid,returner_type,returner_data,returner_index);//,test);
 
+
+logic clk,    // Clock
+	 rst_n;  // synchronous reset active low
+
+	//////////////////////////////////////////////////////////////// timing_controller
+	 burst_states_type [no_of_bursts-1:0] out_burst_state;
+	 r_type [no_of_bursts-1:0] out_burst_type;
+
+	 logic [1:0] [no_of_bursts-1:0] out_burst_address_bank;
+	 logic [1:0] [no_of_bursts-1:0] out_burst_address_bg;
+	 logic [15:0] [no_of_bursts-1:0] out_burst_address_row;
+	 command in_burst_cmd;
+	 logic [$clog2(no_of_bursts)-1:0] in_cmd_index;
+	
+	/////////////////////////////////////////////////////////////// banks arbiter
+	 logic start_new_burst;
+
+	logic arbiter_valid;
+	 address_type in_req_address;
+	logic [data_width -1:0] arbiter_data;
+	logic [read_entries_log -1:0] arbiter_index;
+	logic arbiter_type_temp;
+	
+	/////////////////////////////////////////////////////////////// memory interface
+	 logic CS_n                ;// Chip Select -> active low
+	 logic [13:0] CA           ;// Command / Address Port   
+	 logic CAI                 ;// Command / Address inversion
+	 logic [2:0] DM_n          ;// Data Mask -> byte based 
+	wire [data_width-1:0] DQ  ;// Data Port  
+	wire [2:0] DQS_t , DQS_c ;// Data Strobes (diff pair) // ~Data Strobes (diff pair)
+ 	wire ALERT_n              ; // CRC/Parity error flag
+
+	/////////////////////////////////////////////////////////////// returner interface
+	 logic returner_valid;
+	 r_type returner_type;
+	 logic [data_width-1:0] returner_data;
+	 logic [read_entries_log -1:0] returner_index;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+burst_handler b (
+
+.*
+);
 
 
 
@@ -38,7 +81,7 @@ initial begin
 	test=0;
 	rst_n = 1;
 	arbiter_valid=0;
-	arbiter_type = read;
+	arbiter_type_temp = 0;
 	in_req_address =0;
 	arbiter_index =0;
 
@@ -62,7 +105,7 @@ initial begin
 	@(posedge clk)///////////////////read
 	arbiter_valid=1;
 	@(posedge clk)///////////////////write
-	arbiter_type = write;
+	arbiter_type_temp = 1;
 	@(posedge clk)
 	arbiter_valid=0;
 
