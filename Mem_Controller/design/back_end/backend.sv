b8f10a1b27c76aeb1121034692124f654be1aaca
mohamed-elkhawas <65501370+mohamed-elkhawas@users.noreply.github.com>
2021-07-18 17:27:41 +0200

    Update backend.sv

--- Mem_Controller/design/back_end/backend.sv
+++ Mem_Controller/design/back_end/backend.sv
@@ -52,9 +52,9 @@ module back_end import types_def::*;
     wire burst_states_type [no_of_bursts-1:0] burst_state; // started_filling ,almost_done , full , empty , returning_data
     wire r_type [no_of_bursts-1:0] burst_type;
 
-    logic [1:0] [no_of_bursts-1:0] burst_address_bank;
-    logic [1:0] [no_of_bursts-1:0] burst_address_bg;
-    logic [15:0] [no_of_bursts-1:0] burst_address_row;
+    logic [no_of_bursts-1:0] [1:0] in_burst_address_bank;
+    logic [no_of_bursts-1:0] [1:0] in_burst_address_bg;
+    logic [no_of_bursts-1:0] [15:0] in_burst_address_row;
 
     wire command burst_cmd_o;   // start cmd 
     wire [$clog2(no_of_bursts)-1:0] cmd_index_o;
