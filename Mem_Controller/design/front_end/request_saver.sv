ae2b5a14f322b88ec43f8946f7a8af0155766348
mohamed-elkhawas <65501370+mohamed-elkhawas@users.noreply.github.com>
2021-07-31 05:04:42 +0200

    Update request_saver.sv

--- Mem_Controller/design/front_end/request_saver.sv
+++ Mem_Controller/design/front_end/request_saver.sv
@@ -34,43 +34,45 @@ always_comb begin
 		end
 	end
 
-	if (grant_o[j] == 0 && valid_i == 1 ) begin
+	if (grant_o[j] == 0 && valid_i[j] == 1 ) begin // save the request
 		grant_o2 = 0;
 	end
+  else begin // send normally
+    grant_o2 = 1;
+    valid_i2 = valid_i;
+    index_i2 = index_i;
+    request_i2 = request_i;
+  end
 
-  if (waiting_valid) begin
+  if (waiting_valid) begin // old request waiting
+    
     grant_o2 = 0;
-
     if (grant_o[j]) begin
-       valid_i2 = 1;
+       valid_i2[j] = 1;
        index_i2 = index_waiting;
        request_i2 = waiting;
     end
-    
-  end
-  else begin
-     grant_o2 = 1;
-     valid_i2 = valid_i;
-     index_i2 = index_i;
-     request_i2 = request_i;
   end
 end
 
 always_ff @(posedge clk ) begin
+  
   if(rst_n) begin
 
     if (waiting_valid == 1 && grant_o[j] ==1) begin
        waiting_valid <=0;
     end
 
-    if (grant_o[j] == 0 && valid_i == 1 ) begin
+    if (grant_o[j] == 0 && valid_i[j] == 1 ) begin
       waiting <= request_i;
       index_waiting <= index_i;
       waiting_valid <=1;
     end
  
+  end
+  else begin // reset
+    waiting_valid <=0;
   end 
-
 end
 
 endmodule
