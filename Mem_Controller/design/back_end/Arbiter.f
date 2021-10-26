dcdde1341c2e228304c2d4f32c883a398d413dce
mohamed-elkhawas <65501370+mohamed-elkhawas@users.noreply.github.com>
2021-10-26 18:11:07 +0200

    Delete Mem_Controller directory

--- Mem_Controller/design/back_end/Arbiter.f
+++
@@ -1,5 +0,0 @@
-ROCE/Mem_Controller/design/back_end/Arbiter_tb.sv
-ROCE/Mem_Controller/design/back_end/Arbiter.sv
-ROCE/Mem_Controller/design/back_end/Groups_Fsm.sv
-ROCE/Mem_Controller/design/back_end/Bank_Group_Fsm.sv
-ROCE/Mem_Controller/design/back_end/Data_Path.sv
